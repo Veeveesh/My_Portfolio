3251590b86bf633e221e05e2c48dc66b9848da1e
Veeveesh <vivianihesiaba.ad@gmail.com>
2026-07-07 14:20:26 +0100

    Task for 7th July included

--- Veeveesh_portfolio/admin.py
+++ Veeveesh_portfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Contact, Blog, Skill
+# Register your models here.
+
 
 # Register your models here.
+#@admin.site.register(Contact)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('full_name','subject')
+    search_fields = ('full_name','subject')
+    list_filter = ('created_at',)
+
+admin.site.register(Contact,TaskAdmin)
+admin.site.register([Blog,Skill])
--- Veeveesh_portfolio/models.py
+++ Veeveesh_portfolio/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    full_name = models.CharField(max_length= 1000)
+    subject = models.CharField(max_length=1000)
+    email = models.CharField(max_length=500)
+    message = models.CharField(max_length= 1500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+
+    def __str__(self):
+        return self.full_name
+    
+class Blog(models.Model):
+    title = models.CharField(max_length=1000)
+    Description = models.CharField(max_length=2000)
+    Content = models.CharField(max_length=7000)
+    image = models.ImageField((""), upload_to=None, height_field=None, width_field=None, max_length=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Skill(models.Model):
+    name = models.CharField(max_length=1000)
+    level = models.IntegerField(default=100)
+
+    def __str__(self):
+        return self.name
