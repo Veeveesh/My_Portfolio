cfdd05cfc07226776752f59f2dec94e53baa1420
Veeveesh <vivianihesiaba.ad@gmail.com>
2026-07-08 10:30:18 +0100

    Changed Content to CharField,made migrations,modified .gitignore

--- Veeveesh_portfolio/models.py
+++ Veeveesh_portfolio/models.py
@@ -15,13 +15,13 @@ class Contact(models.Model):
 class Blog(models.Model):
     title = models.CharField(max_length=1000)
     Description = models.CharField(max_length=2000)
-    Content = models.CharField(max_length=7000)
+    Content = models.TextField(max_length=7000)
     image = models.ImageField((""), upload_to='Blog_media', height_field=None, width_field=None, max_length=None)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True,blank=True, null=True)
+    last_updated = models.DateTimeField(auto_now_add=True,blank=True, null=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.title}:{self.Description}"
 
 class Skill(models.Model):
     name = models.CharField(max_length=1000)
